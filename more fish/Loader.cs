7ab42560d3813a451c74c92518734b1203354e55
heavyartillerybot <heavyartillerybot@users.noreply.github.com>
2025-05-12 22:23:10 +0000

    Renaming with your datas

--- more fish/Loader.cs
+++ more fish/Loader.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace WinchModTemplate
+namespace more fish
 {
 	public class Loader
 	{
@@ -9,8 +9,8 @@ namespace WinchModTemplate
 		/// </summary>
 		public static void Initialize()
 		{
-			var gameObject = new GameObject(nameof(WinchModTemplate));
-			gameObject.AddComponent<WinchModTemplate>();
+			var gameObject = new GameObject(nameof(more fish));
+			gameObject.AddComponent<more fish>();
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
---
+++ more fish/more fish.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace more fish
+{
+	public class more fish : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(more fish)} has loaded!");
+		}
+	}
+}
